699f5307f71da0219655b9d3d0d160fd1bb5845a
fabrizio schiano <fschiano@flylab.irisa.fr>
2014-12-19 17:51:48 +0100

    The controller is finally working in Pitch Roll and Yaw.

--- quadrotor_tk_handler/src/Quadrotor_tk_Handler.cpp
+++ quadrotor_tk_handler/src/Quadrotor_tk_Handler.cpp
@@ -23,7 +23,7 @@ _quadrotorMass(0.8),
 // Before changing the following 3 parameters be careful and look closely into the controller which is implemented below
 _att_cutoff(0.2),
 _att_damping(0.8),
-_kp_yaw(0.0),
+_kp_yaw(1),
 _lastReceivedCmdTime(ros::Time::now()),
 _previousTime(ros::Time::now()),
 _integralTermRoll(0.0),
@@ -142,7 +142,7 @@ void Quadrotor_tk_Handler::handleSimulation(){
 	ros::Time now = ros::Time::now();
 	ros::Duration timeStep = now - _previousTime;
 	//DEBUG
-	std::cout<< "DEBUG: TimeStep: " << timeStep << std::endl;
+//	std::cout<< "DEBUG: TimeStep: " << timeStep << std::endl;
 
 	// Position of the quadrotor (x,y,z)
 	Eigen::Matrix<simFloat, 3, 1> position;
@@ -225,7 +225,7 @@ void Quadrotor_tk_Handler::handleSimulation(){
 		// Compute the net force
 		TotforceZ = (simFloat)_tkCommands.thrust;
 
-		Eigen::Matrix< simFloat, 3, 1> rpy = orientation.toRotationMatrix().eulerAngles(0,1,2);
+//		Eigen::Matrix< simFloat, 3, 1> rpy = orientation.toRotationMatrix().eulerAngles(2,1,0);
 		//		Eigen::Matrix< simFloat, 3, 3> rotMatrix = orientation.toRotationMatrix();
 		//		Eigen::Matrix< simFloat, 3, 1> rpy(
 		//				atan2(rotMatrix(3,1),rotMatrix(3,2)),
@@ -236,6 +236,19 @@ void Quadrotor_tk_Handler::handleSimulation(){
 		//				atan2(-rotMatrix(3,1),sqrt(rotMatrix(3,2)*rotMatrix(3,2)+rotMatrix(3,3)*rotMatrix(3,3))),
 		//				atan2(rotMatrix(2,1),rotMatrix(1,1)));
 
+		Eigen::Matrix< simFloat, 3, 3> R = orientation.toRotationMatrix();
+		Eigen::Matrix< simFloat, 3, 1> rpy;
+		rpy(0) = atan2((double)R(2,1), (double)R(2,2));
+		rpy(1) = atan2(-R(2,0), sqrt( R(2,1)*R(2,1) + R(2,2)*R(2,2) ) );
+		rpy(2) = atan2((double)R(1,0),(double)R(0,0));
+
+//		std::cout << "DEBUG: (_tkCommands.roll): " <<  (_tkCommands.roll) <<std::endl;
+//		std::cout << "DEBUG: rpy: " << rpy(0) << "  "<< rpy(1) << "  " << rpy(2) <<std::endl;
+//		std::cout << "DEBUG: R: " << R <<std::endl;
+//
+//		std::cout << "DEBUG: R(3,1): " << R(3,1) <<std::endl;
+//		std::cout << "DEBUG: R(3,2): " << R(3,2) <<std::endl;
+//		std::cout << "DEBUG: R(3,3): " << R(3,3) <<std::endl;
 		// Definition of sin_r,cos_r etc.
 		const double cos_r = cos((double)rpy(0));
 		//std::cout << "DEBUG: cos_r: " <<  cos_r <<std::endl;
@@ -257,8 +270,8 @@ void Quadrotor_tk_Handler::handleSimulation(){
 		const simFloat kd_att = _att_damping;
 
 		// Errors
-		const simFloat errorRoll = sin(_tkCommands.roll)-sin(rpy(0));
-		const simFloat errorPitch = sin(_tkCommands.pitch)-sin(rpy(1));
+		const simFloat errorRoll = (_tkCommands.roll)-(rpy(0));
+		const simFloat errorPitch = (_tkCommands.pitch)-(rpy(1));
 
 		// Computaton of the integral terms
 		_integralTermRoll = _integralTermRoll + timeStep.toSec() * errorRoll;
@@ -267,28 +280,28 @@ void Quadrotor_tk_Handler::handleSimulation(){
 		if(_integralTermPitch<0.0001)_integralTermPitch=0.0;
 
 		//DEBUG
-		std::cout<< "DEBUG: _integralTermRoll: " << _integralTermRoll << std::endl;
-		std::cout<< "DEBUG: _integralTermPitch: " << _integralTermPitch << std::endl;
+//		std::cout<< "DEBUG: _integralTermRoll: " << _integralTermRoll << std::endl;
+//		std::cout<< "DEBUG: _integralTermPitch: " << _integralTermPitch << std::endl;
 
 
 
 
 		Eigen::Matrix< simFloat, 3, 1> rpyTorque(
 				//roll
-				kp_att*(errorRoll) + kd_att*(0-rpyRate(0))+0.001*_integralTermRoll,
+				kp_att*(errorRoll) + kd_att*(0-rpyRate(0))+0*0.001*_integralTermRoll,
 				//pitch
 //				0.1*kp_att*(errorPitch) + 0.02*kd_att*(0-rpyRate(1))+0*0.002*_integralTermPitch,
-				0*kp_att*(errorPitch) + 0.02*kd_att*(0-rpyRate(1))+0*0.002*_integralTermPitch,
+				kp_att*(errorPitch) + kd_att*(0-rpyRate(1))+0*0.002*_integralTermPitch,
 				//yaw
-				_kp_yaw*(- rpyRate(2)));  //sign is the opposite of the yaw angular velocity
+				_kp_yaw*(_tkCommands.yaw- rpyRate(2)));  //sign is the opposite of the yaw angular velocity
 
 		//DEBUG
-		std::cout << "DEBUG: sin(_tkCommands.roll): " <<  sin(_tkCommands.roll) <<std::endl;
-		std::cout << "DEBUG: sin(rpy(0)): " << sin(rpy(0)) <<std::endl;
-		std::cout << "DEBUG: sin(_tkCommands.pitch): " <<  sin(_tkCommands.pitch) <<std::endl;
-		std::cout << "DEBUG: sin(rpy(1)): " << sin(rpy(1)) <<std::endl;
-		std::cout << "DEBUG: errorRoll: " << errorRoll <<std::endl;
-		std::cout << "DEBUG: errorPitch: " << errorPitch <<std::endl;
+//		std::cout << "DEBUG: (_tkCommands.roll): " <<  (_tkCommands.roll) <<std::endl;
+//		std::cout << "DEBUG: rpy: " << rpy(0) << "  "<< rpy(1) << "  " << rpy(2) <<std::endl;
+//		std::cout << "DEBUG: sin(_tkCommands.pitch): " <<  sin(_tkCommands.pitch) <<std::endl;
+//		std::cout << "DEBUG: sin(rpy(1)): " << sin(rpy(1)) <<std::endl;
+//		std::cout << "DEBUG: errorRoll: " << errorRoll <<std::endl;
+//		std::cout << "DEBUG: errorPitch: " << errorPitch <<std::endl;
 
 		rpyTorque = nwuToNed*orientation*rpyTorque; //rotate torque to world frame
 		//const Eigen::Matrix< simFloat, 3, 1> worldForce = nwuToNed*Eigen::Matrix< simFloat, 3, 1>(0.0,0.0,(simFloat)_tkCommands.thrust);
@@ -297,13 +310,13 @@ void Quadrotor_tk_Handler::handleSimulation(){
 
 
 		// To Delete Probably
-		if(worldForce(0)<0.001 && worldForce(0)>-0.001)worldForce(0)=0;
-		if(worldForce(1)<0.001 && worldForce(1)>-0.001)worldForce(1)=0;
+//		if(worldForce(0)<0.001 && worldForce(0)>-0.001)worldForce(0)=0;
+//		if(worldForce(1)<0.001 && worldForce(1)>-0.001)worldForce(1)=0;
 
 
 		//DEBUG torques and forces
-		std::cout << "DEBUG: rpyTorque: " << rpyTorque(0) <<"|"<< rpyTorque(1) <<"|"<< rpyTorque(2) <<"|" <<std::endl;
-		std::cout << "DEBUG: forces: " << worldForce(0) <<"|"<< worldForce(1) <<"|"<< worldForce(2) <<"|" <<std::endl;
+//		std::cout << "DEBUG: rpyTorque: " << rpyTorque(0) <<"|"<< rpyTorque(1) <<"|"<< rpyTorque(2) <<"|" <<std::endl;
+//		std::cout << "DEBUG: forces: " << worldForce(0) <<"|"<< worldForce(1) <<"|"<< worldForce(2) <<"|" <<std::endl;
 
 		//        std::stringstream ss;
 		//        ss << "applying force : [" << worldForce.transpose() << std::endl;
